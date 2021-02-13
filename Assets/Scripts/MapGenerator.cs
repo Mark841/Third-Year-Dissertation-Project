178d64f00e9d00273959669698f61d6b5c66a94d
Mark841 <lumb.mark@yahoo.co.uk>
2021-02-13 19:55:16 +0000

    First commit

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode { NoiseMap, ColourMap, Mesh };
+    public DrawMode drawMode;
+
+    // Must be 241 or below in size otherwise the LevelOfDetail functionality breaks as the list index goes out of bounds
+    const int chunkSize = 241;
+    // The higher the level of detail goes the smaller the chunk size must be, 241 is largest it can be for LoD 6 if more LoD then chunk size must be decreased
+    [Range(0, 6)]
+    public int levelOfDetail;
+    [Range(10.0f, 1000.0f)]
+    public float noiseScale;
+
+    [Range(1, 10)]
+    public int octaves;
+    [Range(0, 1)]
+    public float persistence;
+    [Range(1.0f, 60.0f)]
+    public float lacunarity;
+    const float DISTORT_STRENGTH = 1.25f;
+    [Range(0.0f, 15.0f)]
+    public float roughness = 2.0f;
+    public Vector2 xWarpOffset = new Vector2(50.0f, 50.0f);
+    public Vector2 yWarpOffset = new Vector2(20.0f, 30.0f);
+
+    public int seed;
+    public Vector2 offset;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
+    public bool normalise;
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = NoiseGenerator.GenerateNoiseMap(chunkSize, chunkSize, seed, noiseScale, octaves, persistence, lacunarity, DISTORT_STRENGTH, roughness, offset, xWarpOffset, yWarpOffset, normalise);
+
+        Color[] colourMap = new Color[chunkSize * chunkSize];
+
+        for (int y = 0; y < chunkSize; y++)
+        {
+            for (int x = 0; x < chunkSize; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i=0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * chunkSize + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.drawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColourMap)
+        {
+            display.drawTexture(TextureGenerator.TextureFromColourMap(colourMap, chunkSize, chunkSize));
+        }
+        else if (drawMode == DrawMode.Mesh)
+        {
+            display.drawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColourMap(colourMap, chunkSize, chunkSize));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (levelOfDetail < 0)
+        {
+            levelOfDetail = 0;
+        }
+        if (noiseScale < 10)
+        {
+            noiseScale = 10.0f;
+        }
+        if (roughness < 0)
+        {
+            roughness = 0.0f;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1.0f;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
+}
---
+++ Assets/Scripts/NoiseGenerator.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NoiseGenerator
+{
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistence, float lacunarity, float DISTORT_STRENGTH, float roughness, Vector2 offset, Vector2 xWarpOffset, Vector2 yWarpOffset, bool normalise)
+    {
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        // Pseudo Random Number Generator
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfWidth = mapWidth / 2f;
+        float halfHeight = mapHeight / 2f;
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+                    float xSample = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
+                    float ySample = (y - halfHeight) / scale * frequency + octaveOffsets[i].y;
+
+                    float xWarping = DISTORT_STRENGTH * Mathf.PerlinNoise((xSample + xWarpOffset.x) * roughness, (ySample + xWarpOffset.y) * roughness);
+                    float yWarping = DISTORT_STRENGTH * Mathf.PerlinNoise((xSample + yWarpOffset.x) * roughness, (ySample + yWarpOffset.y) * roughness);
+
+                    float noiseValue = Mathf.PerlinNoise(xSample + xWarping, ySample + yWarping) * 2 - 1;
+                    noiseHeight += (noiseValue * amplitude);
+
+                    amplitude *= persistence;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    // Set the highest point of the terrain
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    // Set the lowest point of the terrain
+                    minNoiseHeight = noiseHeight;
+                }
+
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        if (normalise)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    // normalises the noise map using the highest and lowest heights of terrain
+                    noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+                }
+            }
+        }
+
+        return noiseMap;
+    }
+}
